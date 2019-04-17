c0864928d2dbfc258e1335b5bded623dfb68bb4b
Mira <felipemira12@hotmail.com>
2019-04-17 15:03:10 -0300

    Program tested and working

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,24 @@ int main(){
     screen.printf("Brighter(+) or Dimmer(-)\n");
     action = screen.getc();
     if(action == '+'){
+      if(led[2]>=1.0){
       led[2] = led[2]+0.05;
       led[0] = 1;
       timeout.attach(&desligar_leds, 0.1);
+      }
+      else{
+        screen.printf("LED at max power\n");
+      }
     }
     if(action == '-'){
+      if(led[2]<=0.0){
       led[2] = led[2]-0.05;
       led[1] = 1;
       timeout.attach(&desligar_leds, 0.1);
+      }
+      else{
+        screen.printf("LED turned off\n");
+      }
     }
     else{
       printf("Invalid character!\n");
@@ -29,5 +39,5 @@ int main(){
 
 void desligar_leds(){
   led[0]=0;
-  led[1]=1;
+  led[1]=0;
 }
